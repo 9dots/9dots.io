73482369242b27780bba2600d77e51038c042870
Daniel Leavitt <danleavitt0@gmail.com>
2014-07-29 11:47:43 -0700

    prerender

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(express.logger('dev'));
 app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 
-
 // mount
 
 // serves markdown pages at /markdown/pages
@@ -31,8 +30,6 @@ app.use(require('./lib/upload'));
 //app.post('/upload', function() {
 //	console.log('upload');
 //})
-
-app.use(require('prerender-node').set('prerenderToken', 'toId3gs33teeQt7Ln7XR'));
 
 // boot app
 app.use(require('boot'));
